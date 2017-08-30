926ab525d657d2b232b11b5697f8e52d1c089fba
andyk1278 <andyk1278@gmail.com>
2017-08-30 14:42:48 -0700

    starting users app

--- feelings/settings.py
+++ feelings/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'thoughts',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'),]
+
+LOGIN_URL = 'users:login'
